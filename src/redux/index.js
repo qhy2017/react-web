c9d922a43b5e7dbd34acc00c3553ba1bbce437a4
qhy2017 <1242359237@qq.com>
2022-08-11 14:33:32 +0800

    commit

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,19 +1,9 @@
 import { combineReducers } from 'redux'
 import { configureStore  } from '@reduxjs/toolkit'
-import { Outlet, NavLink, useNavigate,useLocation  } from "react-router-dom";
-import {createBrowserHistory,createHashHistory} from "history";
-
-let history = createBrowserHistory()
-let hashHistory = createHashHistory()
- hashHistory.listen(function() {
-    console.log("hashHistory")
-    console.log(arguments)
- });
  
 function counter(state = 0, action) {
 	switch (action.type) {
 		case 'INCREMENT':
-            hashHistory.push("/link");
 			return state + 1;
 		case 'DECREMENT':
 			return state - 1;
@@ -31,12 +21,6 @@ const store = configureStore({
     reducer: combines
 }) 
 
-store.subscribe(function(item) {
-    console.log(store.getState())
-})
-store.dispatch({
-	type: 'INCREMENT'
-});
 export default store;
 
 export const navigate = ()=> {  };
